4d6d1889ee89c884d9b8a54ff2b7eea7fba5db6d
Calvin Dea <calvindea@Calvins-MacBook-Air.local>
2024-12-06 19:31:26 -0500

    Added some touch ups and missing value estimates

--- Main.py
+++ Main.py
@@ -6,13 +6,14 @@ scripts = [
     "LDA.py",
     "Logistic_Regression.py",
     "KNN.py",
-    "SVM.py"
+    "SVM.py",
+    "Missing_Value_Estimate.py"
 ]
 
 def run_scripts(script_list):
     for script in script_list:
         try:
-            result = subprocess.run(["python", script], capture_output=True, text=True, check=True)
+            result = subprocess.run(["python3", script], capture_output=True, text=True, check=True)
             print(result.stdout)  
         except subprocess.CalledProcessError as e:
             print(e.stderr) 
